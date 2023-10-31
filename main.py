926d43a9a4820328770932d356909b27b11352f2
kshamavv <140036252+kshamavv@users.noreply.github.com>
2023-10-31 11:26:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     someVarFromEnv = os.environ.get('someVar')
-    return 'I have changed the text here for testing: {}!\n second line'.format(someVarFromEnv)
+    return 'Hi! Welcome to this page'.format(someVarFromEnv)
 
 if __name__ == "__main__":
     app.debug = True
